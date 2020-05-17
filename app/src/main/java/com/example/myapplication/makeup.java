746c042a6393d7cd9cd7cf1714a770e2872c18d2
SawsaneHafsati <SawsaneHaf@gmail.com>
2020-05-17 18:13:13 +0200

    API

---
+++ app/src/main/java/com/example/myapplication/makeup.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+public class makeup {
+
+    private String name;
+    private String brand;
+    private String price;
+    private String color;
+    private String imgURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getImgURL() {
+        return imgURL;
+    }
+}
